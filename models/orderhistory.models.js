adbbb36381cb36ac61501dda2ea567f60b06c732
abhra <patra.abhra97@gmail.com>
2026-01-13 16:18:24 +0530

    schema updated

--- models/orderhistory.models.js
+++ models/orderhistory.models.js
@@ -1,16 +1,47 @@
 const mongoose = require("mongoose");
 
 const orderhistorySchema = new mongoose.Schema({
-    orderItemHeader: String,
-    orderItemQuantity: Number,
-    orderItemPrice: Number,
-    selectedAddress: String,
-    totalamount: Number,
+    items: {
+        productId: {
+            type: String,
+            required: true,
+        },
+        title: {
+            type: String,
+            required: true,
+        },
+        price: {
+            type: Number,
+            required: true,
+        },
+        quantity: {
+            type: Number,
+            required: true,
+        },
+        variant: {
+            color: String,
+            ram: String,
+            storage: String,
+        },
+    },
+    totalQuantity: {
+        type: Number,
+        required: true,
+    },
+    address: {
+        type: String,
+        required: true,
+    },
+    totalAmount: {
+        type: Number,
+        required: true,
+    },
 },
-{
-    timestamps: true,
-});
+    {
+        timestamps: true,
+    }
+);
 
 const orderhistoryData = mongoose.model("orderhistoryData", orderhistorySchema);
 
-module.exports = orderhistoryData ;
+module.exports = orderhistoryData;
